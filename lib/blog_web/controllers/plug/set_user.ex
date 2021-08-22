26edb8cbbad1b8c761daceaae4b2467833df947e
Itor  Isaias <info@insignaconsultoria.com.br>
2021-08-22 10:55:46 -0300

    create plug custom

---
+++ lib/blog_web/controllers/plug/set_user.ex
@@ -0,0 +1,19 @@
+defmodule BlogWeb.Plug.SetUser do
+  import Plug.Conn
+  alias Blog.Accounts
+
+  def init(_opts) do
+  end
+
+  def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
+
+    cond do
+      user = user_id && Accounts.get_user!(user_id) ->
+        assign(conn, :user, user)
+
+      true ->
+        assign(conn, :user, nil)
+    end
+  end
+end
